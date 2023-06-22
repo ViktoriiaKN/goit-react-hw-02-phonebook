9757badd368bf5deb178542f7c95e58c49300d52
ViktoriiaKN <113258083+ViktoriiaKN@users.noreply.github.com>
2023-06-22 18:13:13 +0200

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,35 @@ import { Component } from 'react';
 
 export class App extends Component {
   state = { contacts: [], name: '' };
+
+  handleChange = evt => {
+    this.setState();
+  };
+
+  onSubmit = () => {};
+
   render() {
+    const { contacts, name } = this.state;
     return (
       <>
-        <form></form>
+        <form onSubmit={this.onSubmit}>
+          <label>
+            <input
+              onChange={this.handleChange}
+              value={name}
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+            <button type="submit">ADD CONTACT</button>
+          </label>
+        </form>
         <ul>
-          <li>lalala</li>
+          {contacts.map(contact => (
+            <li key={contact.id}>{contact.name}</li>
+          ))}
         </ul>
       </>
     );
